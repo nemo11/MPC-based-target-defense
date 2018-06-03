682d7f065d77e96d016ecc86005c9c68435b2354
nemo11 <nishantmohanty18@gmail.com>
2018-06-03 23:28:03 +0530

    symbolic evaluation being done; indentations managed

--- VirtualArena_MatlabROS/turtlesim_MPC/Two_agent_MPC.m
+++ VirtualArena_MatlabROS/turtlesim_MPC/Two_agent_MPC.m
@@ -1,58 +1,64 @@
+%% Using MPC controller to decrease the distance between two turtle.
+%% Turtle1 moving with constant velocity, Turtle2 using control variable from MPC to minimize the given cost function.
 
 clc; close all; clear all;
 
-mysub2 = rossubscriber('/turtle1/pose');
-mypub2 = rospublisher('/turtle1/cmd_vel');
+% publisher,subscriber and other required initializations....
 
-mysub1 = rossubscriber('/turtle2/pose');
-mypub1 = rospublisher('/turtle2/cmd_vel');
+turtle1_Pose_Subscriber = rossubscriber('/turtle1/pose');
+turtle1_Vel_Publisher = rospublisher('/turtle1/cmd_vel');
 
-D1 = receive(mysub1 , 10);
-D2 = receive(mysub2 , 10);
+turtle2_Pose_Subscriber = rossubscriber('/turtle2/pose');
+turtle2_Vel_Publisher = rospublisher('/turtle2/cmd_vel');
+
+turtle1_Pose_Data = receive(turtle1_Pose_Subscriber , 10);
+turtle2_Pose_Data = receive(turtle2_Pose_Subscriber , 10);
 
 dt = 1;
 
+realSystem = RealVehicleROS_V4(turtle1_Pose_Subscriber,turtle1_Vel_Publisher,turtle2_Pose_Subscriber,turtle2_Vel_Publisher);    %system initialization.
 
-%% Unicycle Model
+%% Angle correction for turtlesim in order to constain it between -pi to pi 
+theta = turtle2_Pose_Data.Theta;
 
-realSystem = RealVehicleROS_V4(mysub1,mypub1,mysub2,mypub2);
-
-theta = D2.Theta;
 if( theta > 3.14 )
    theta = theta - 2*3.14;
 end
 if( theta < -3.14 )
    theta = theta + 2*3.14;
 end
+%% ........
 
-realSystem.initialCondition = {double([D2.X;
-                                D2.Y;
+realSystem.initialCondition = {double([turtle2_Pose_Data.X;
+                                turtle2_Pose_Data.Y;
                                 theta;
-                                D1.X;
-                                D1.Y;
-                                sqrt((D1.X - D2.X)^2 + (D1.Y - D2.Y)^2);
-                                (atan2((D1.Y - D2.Y),(D1.X - D2.X)))])};
+                                turtle1_Pose_Data.X;
+                                turtle1_Pose_Data.Y;
+                                sqrt((turtle1_Pose_Data.X - turtle2_Pose_Data.X)^2 + (turtle1_Pose_Data.Y - turtle2_Pose_Data.Y)^2);
+                                (atan2((turtle1_Pose_Data.Y - turtle2_Pose_Data.Y),(turtle1_Pose_Data.X - turtle2_Pose_Data.X)))])};
                                                     
 mpcOp = ICtMpcOp( ...
-    'System'               , realSystem,...
-    'HorizonLength'        , 2*dt,...
-    'StageCost'            , @(t,x,u,varargin) -x(6));
+                'System'               , realSystem,...
+                'HorizonLength'        , 2*dt,...
+                'StageCost'            , @(t,x,u,varargin) -x(6));
 
 dtMpcOp      = DiscretizedMpcOp(mpcOp,dt);
 
 dtRealSystem = DiscretizedSystem(realSystem,dt);
 
 dtRealSystem.controller = MpcController(...
-    'MpcOp'       , dtMpcOp ,...
-    'MpcOpSolver' , FminconMpcOpSolver('MpcOp', dtMpcOp,'UseSymbolicEvaluation',1) ...
-    );
+                                      'MpcOp'       , dtMpcOp ,...
+                                      'MpcOpSolver' , FminconMpcOpSolver('MpcOp', dtMpcOp,'UseSymbolicEvaluation',1) ...
+                                       );
 
+%% VirtualArena Object defined.                                   
+                                   
 va = VirtualArena(dtRealSystem,...
-    'StoppingCriteria'  , @(t,sysList)sqrt(((dtRealSystem.x(4)-dtRealSystem.x(1))^2+(dtRealSystem.x(5)-dtRealSystem.x(2))^2))<0.4 ,...
-    'DiscretizationStep', dt ,...
-    'RealTime',1/dt , ...
-    'PlottingStep'      , 1/dt ,...
-    'StepPlotFunction'  , @M2StepPlotFunction ...
-    );
-
+                  'StoppingCriteria'  , @(t,sysList)sqrt(((dtRealSystem.x(4)-dtRealSystem.x(1))^2+(dtRealSystem.x(5)-dtRealSystem.x(2))^2))<0.4 ,...
+                  'DiscretizationStep', dt ,...
+                  'RealTime',1/dt , ...
+                  'PlottingStep'      , 1/dt ,...
+                  'StepPlotFunction'  , @M2StepPlotFunction ...
+                 );
+             
 log = va.run();
